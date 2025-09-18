fa4e890ce5ae132fb1bc38ded406fc58ce1026d0
Sabari1239 <sabarivasan1239@gmail.com>
2025-09-18 12:58:56 +0530

    file changes

--- src/electron.js
+++ src/electron.js
@@ -1,20 +1,19 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+import { app, BrowserWindow } from "electron";
+import { join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: join(__dirname, "preload.js"), // optional
     },
   });
 
-  // Load React build output
-  win.loadFile(path.join(__dirname, "dist", "index.html"));
-
-  // Uncomment to open DevTools
-  // win.webContents.openDevTools();
+  win.loadFile(join(__dirname, "dist", "index.html"));
 }
 
 app.whenReady().then(() => {
